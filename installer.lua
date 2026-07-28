7bc55862365f9c21fdd9eef5ed2c74fd7808eb71
ArtMinerCZ <62687944+ArtMinerCZ@users.noreply.github.com>
2026-07-28 20:33:47 +0200

    Intranet Explorer installer

---
+++ installer.lua
@@ -0,0 +1,15 @@
+if not fs.exists("explorer") then
+    fs.makeDir("explorer")
+end
+
+if not fs.exists("basalt.lua") then
+    shell.run("wget run https://basalt.madefor.cc/2.5/install.lua")
+end
+
+shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/explorer/array.lua explorer/array.lua")
+shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/explorer/mtml.lua explorer/mtml.lua")
+
+shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/explorer/render.lua explorer/render.lua")
+
+shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/explorer/IntranetExplorer.lua explorer/IntranetExplorer.lua")
+
